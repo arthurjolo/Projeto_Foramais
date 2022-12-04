a996782b21bc638dc7ce2d1b86b080f0e96724a8
unknown <arthurjolo@gmail.com>
2022-12-04 20:40:12 -0300

    união recebe multiplos arquivos

--- uniao.py
+++ uniao.py
@@ -2,9 +2,10 @@ from enum import auto
 import math
 from automato_finito import AutomatoFinito
 
-from determinizar import determinizar 
+from determinizar import determinizar
+from regex_para_afd import regex_para_afd
 
-def uniao(automato1, automato2):
+def uniao(automatos):
     estado_inicial = 0
     estados = [0]
     alfabeto = []
@@ -15,35 +16,40 @@ def uniao(automato1, automato2):
     #automato1 = AutomatoFinito(automato1_txt)
     #automato2 = AutomatoFinito(automato2_txt)
     #construindo os estados do novo automato
-    n_estados = automato1.get_n_estados() + automato2.get_n_estados() + 1
+    n_estados = 1
     #contruindo o alfabeto do novo automato
     alfabeto.append("&")
-    for l in automato1.get_alfabeto():
-        if not(l in alfabeto):
-            alfabeto.append(l)
+    for automato in automatos:
+        for l in automato.get_alfabeto():
+            if not(l in alfabeto):
+                alfabeto.append(l)
+        n_estados += automato.get_n_estados()
 
-    for l in automato2.get_alfabeto():
-        if not(l in alfabeto):
-            alfabeto.append(l)
+    #for l in automato2.get_alfabeto():
+    #    if not(l in alfabeto):
+    #        alfabeto.append(l)
         
     #contruindo estados finais do novo automato
-    if list(automato1.lable_estados_finais.keys()) == []:
-        for f in automato1.get_estados_finais():
-            estados_finais.append(f+1)
-            lable_estados_finais[f+1] = [automato1.nome]
-    else:
-        for f in automato1.get_estados_finais():
-            estados_finais.append(f+1)
-            lable_estados_finais[f+1] = automato1.lable_estados_finais[f][:]
+    ajuste = 1
+    for automato in automatos:
+        if list(automato.lable_estados_finais.keys()) == []:
+            for f in automato.get_estados_finais():
+                estados_finais.append(f+ajuste)
+                lable_estados_finais[f+ajuste] = [automato.nome]
+        else:
+            for f in automato.get_estados_finais():
+                estados_finais.append(f+ajuste)
+                lable_estados_finais[f+ajuste] = automato.lable_estados_finais[f][:]
+        ajuste += automato.get_n_estados()
     
-    if list(automato2.lable_estados_finais.keys()) == []:
-        for f in automato2.get_estados_finais():
-            estados_finais.append(f+ automato1.get_n_estados() +1)
-            lable_estados_finais[f+ automato1.get_n_estados() +1] = [automato2.nome]
-    else:
-        for f in automato2.get_estados_finais():
-            estados_finais.append(f+ automato1.get_n_estados() +1)
-            lable_estados_finais[f+ automato1.get_n_estados() +1] = automato2.lable_estados_finais[f][:]
+    #if list(automato2.lable_estados_finais.keys()) == []:
+    #    for f in automato2.get_estados_finais():
+    #        estados_finais.append(f+ automato1.get_n_estados() +1)
+    #        lable_estados_finais[f+ automato1.get_n_estados() +1] = [automato2.nome]
+    #else:
+    #    for f in automato2.get_estados_finais():
+    #        estados_finais.append(f+ automato1.get_n_estados() +1)
+    #        lable_estados_finais[f+ automato1.get_n_estados() +1] = automato2.lable_estados_finais[f][:]
 
     #for f in automato2.get_estados_finais():
     #    estados_finais.append(f + automato1.get_n_estados() + 1)
@@ -57,16 +63,25 @@ def uniao(automato1, automato2):
         tabela_de_transicao.append(linha)
     
     #transição do estado inicial(0) por epsilon(posição 0 do alfabeto) = estados iniciais de a1 e a2
-    tabela_de_transicao[0][0] = [1, automato1.get_n_estados() + 1]
+    ajuste = 1
+    #tabela_de_transicao[0][0] = [1, automato1.get_n_estados() + 1]
+    tabela_de_transicao[0][0] = []
+    for i in range(len(automatos)):
+        tabela_de_transicao[0][0] += [ajuste]
+        ajuste += automatos[i].get_n_estados()
+    print(tabela_de_transicao)
+    automato_n = 0
+    automato = automatos[0]
+    ajuste = 1
+    print(estados_finais)
     for i in range(1, n_estados):
-        if (i <= automato1.get_n_estados()):
-            automato = automato1
-            estado = i - 1
-            ajuste = 1
+        if (i < ajuste + automato.get_n_estados()):
+            automato = automatos[automato_n]
         else:
-            automato = automato2
-            estado = i - 1 - automato1.get_n_estados()
-            ajuste = 1 + automato1.get_n_estados()
+            automato_n += 1
+            ajuste += automato.get_n_estados()
+            automato = automatos[automato_n]
+        estado = i - ajuste
         alfabeto_automato = automato.get_alfabeto()
         
         automato_trasicoes = automato.get_tabela_de_transicoes()
@@ -77,8 +92,8 @@ def uniao(automato1, automato2):
             else:
                 tabela_de_transicao[i][j] =math.inf
     
-    escrever_afd("uniao_"+automato1.nome+"_"+automato2.nome, n_estados, estado_inicial, estados_finais, alfabeto, tabela_de_transicao)
-    return determinizar("uniao_"+automato1.nome+"_"+automato2.nome, n_estados ,estado_inicial, alfabeto, tabela_de_transicao, estados_finais, lable_estados_finais)
+    escrever_afd("uniao_", n_estados, estado_inicial, estados_finais, alfabeto, tabela_de_transicao)
+    return determinizar("uniao_", n_estados ,estado_inicial, alfabeto, tabela_de_transicao, estados_finais, lable_estados_finais)
 
 def escrever_afd(nome, n_estados, estado_inicial, estados_finais, alfabeto, tabela_transicoes):
     nome_arquivo = f"./uniao_afd/afd_"+ nome +".txt"
